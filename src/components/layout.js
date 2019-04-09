dcc4db330a7dd6fc5bb8f791e9894c3dd45aedb0
Frederic Woelffel <frederic.woelffel@ihu-strasbourg.eu>
2019-04-09 13:59:01 +0200

    feat: integrate bulma
    wip

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,9 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import { graphql, StaticQuery } from "gatsby"
+import "./style.scss"
+import { Container, Hero, HeroBody, HeroFooter, HeroHeader, Title } from "bloomer"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -25,25 +24,20 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-            <p>Last built {new Date(data.site.buildTime).toLocaleDateString()} at {new Date(data.site.buildTime).toLocaleTimeString()}</p>
-          </footer>
-        </div>
-      </>
+      <Hero isFullHeight={true} isColor='info' isSize='medium'>
+        <HeroHeader>
+        </HeroHeader>
+
+        <HeroBody>
+          <Container hasTextAlign='centered'>
+            <Title>Title</Title>
+          </Container>
+        </HeroBody>
+
+        <HeroFooter>
+
+        </HeroFooter>
+      </Hero>
     )}
   />
 )
