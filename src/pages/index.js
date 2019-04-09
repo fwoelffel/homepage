e50619cd50019aa37fac065e91e6477bfe08e099
Frederic Woelffel <frederic.woelffel@ihu-strasbourg.eu>
2019-04-09 08:57:20 +0200

    feat: clean existing content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Home" keywords={[]} />
+    <ul>
+      <li><a href={`https://github.com/fwoelffel`}>Github</a></li>
+      <li><a href={`https://www.linkedin.com/in/fredericwoelffel/`}>Linkedin</a></li>
+      <li><a href={`https://twitter.com/fwoelffel`}>Twitter</a></li>
+      <li><a href={`https://frdricwoelffel.typeform.com/to/TgRdLj`}>Get in touch</a></li>
+    </ul>
+    <p>This is still under construction, come back later!</p>
   </Layout>
 )
 
