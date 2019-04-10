d0c5c3a807db01d5f547c894942cda02fca12e9d
Frederic Woelffel <frederic.woelffel@ihu-strasbourg.eu>
2019-04-10 18:59:19 +0200

    feat(hero): add social links

--- src/components/hero.component.js
+++ src/components/hero.component.js
@@ -1,6 +1,9 @@
 import { graphql, StaticQuery } from "gatsby"
-import { Container, Hero, Title } from "rbx"
+import { Block, Container, Hero, Title } from "rbx"
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
 const HeroComponent = () => (
   <StaticQuery
@@ -19,10 +22,26 @@ const HeroComponent = () => (
       <Hero gradient color={"primary"} size={"fullheight"}>
         <Hero.Body>
           <Container textAlign={"centered"}>
-            <Title size={1}>{data.site.siteMetadata.title}</Title>
-            <Title subtitle>
-              {data.site.siteMetadata.description}
-            </Title>
+            <Block>
+              <Title size={1}>{data.site.siteMetadata.title}</Title>
+              <Title subtitle>
+                {data.site.siteMetadata.description}
+              </Title>
+            </Block>
+            <Block>
+              <a className={"social"} href={"https://www.linkedin.com/in/fredericwoelffel/"} target={"_blank"}>
+                <FontAwesomeIcon icon={faLinkedin} size="4x" />
+              </a>
+              <a className={"social"} href={"https://twitter.com/fwoelffel"} target={"_blank"}>
+                <FontAwesomeIcon icon={faTwitter} size="4x" />
+              </a>
+              <a className={"social"} href={"https://github.com/fwoelffel"} target={"_blank"}>
+                <FontAwesomeIcon icon={faGithub} size="4x" />
+              </a>
+              <a className={"social"} href={"https://frdricwoelffel.typeform.com/to/TgRdLj"} target={"_blank"}>
+                <FontAwesomeIcon icon={faEnvelope} size="4x" />
+              </a>
+            </Block>
           </Container>
         </Hero.Body>
       </Hero>
