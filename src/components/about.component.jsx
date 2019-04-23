faf0ef62c971a76be01cd86535333340b854aa20
Frederic Woelffel <frederic.woelffel@ihu-strasbourg.eu>
2019-04-23 13:54:59 +0200

    feat(about): initialize the about section

---
+++ src/components/about.component.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Container, Content, Section, Title } from "rbx"
+
+const AboutComponent = () =>
+  <Section>
+    <Container>
+      <Title as={"h2"}>About me</Title>
+      <Content textAlign={"justified"}>
+        <p>Cat ipsum dolor sit amet, sphynx. Tom munchkin ragdoll so mouser and norwegian forest for grimalkin tiger. Ragdoll. Leopard mouser, himalayan for burmese. Panther british shorthair. Thai balinese and lion. Tabby cheetah american shorthair or tiger but ocicat or tomcat mouser. American shorthair persian scottish fold lynx. Savannah russian blue yet grimalkin or american bobtail turkish angora singapura lion. Tiger. American bobtail balinese tom for munchkin yet maine coon ocicat. Lynx kitten or singapura so savannah yet siamese. Bobcat. Maine coon grimalkin himalayan, persian but grimalkin. Kitty burmese cornish rex scottish fold savannah singapura so singapura. Leopard abyssinian . Scottish fold. Russian blue tomcat turkish angora havana brown but tiger turkish angora. Grimalkin.</p>
+        <p>British shorthair manx kitty. Bobcat malkin so egyptian mau. Egyptian mau singapura but munchkin but lynx and turkish angora. Singapura. Tom thai birman but lion tomcat. Ocicat egyptian mau or malkin yet bengal, for norwegian forest. Mouser siberian thai maine coon russian blue balinese devonshire rex. Bobcat tom but bobcat. Kitty tom and cheetah, tabby. Maine coon russian blue tomcat lion ocelot yet havana brown. Balinese . Balinese cheetah or ragdoll so bombay tabby. Ocicat himalayan singapura for grimalkin or himalayan malkin ocicat. Norwegian forest. Burmese savannah bengal manx and sphynx devonshire rex. Havana brown scottish fold but puma balinese yet ocelot turkish angora birman. Burmese american shorthair devonshire rex manx. Manx havana brown, american bobtail and ocelot and himalayan so kitty. Egyptian mau siamese but norwegian forest, lynx norwegian forest but himalayan. Donskoy balinese puma birman. Bengal cheetah. Balinese thai devonshire rex but puma but bengal for savannah.</p>
+      </Content>
+    </Container>
+  </Section>
+
+export default AboutComponent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroComponent from "../components/hero.component"
 import FooterComponent from "../components/footer.component"
+import AboutComponent from "../components/about.component"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[]} />
     <HeroComponent/>
+    <AboutComponent/>
     <FooterComponent/>
   </Layout>
 )
