50a7a8ca6f45a7220d42c3b9242cf169d47c7735
Frederic Woelffel <hello@fwoelffel.me>
2019-05-09 23:50:17 +0200

    refactor(hero): make the hero component fully functional

--- src/components/hero.component.js
+++ src/components/hero.component.js
@@ -1,47 +1,41 @@
-import { graphql, StaticQuery } from "gatsby"
 import React from "react"
 import { FaEnvelope, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa"
 
-const HeroComponent = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          buildTime
-          siteMetadata {
-            title,
-            description
-          }
-        }
-      }
-    `}
-    render={data => (
-      <section class="hero is-fullheight is-bold is-info">
-        <div class="hero-body has-text-centered">
-          <div class="container">
-            <h1 class="title is-1">{data.site.siteMetadata.title}</h1>
-            <h2 class="subtitle">{data.site.siteMetadata.description}</h2>
-            <div class="is-block">
-              <a className="social" href="https://www.linkedin.com/in/fredericwoelffel/" target="_blank"
-                 rel="noopener noreferrer">
-                <FaLinkedin size="4em"/>
-              </a>
-              <a className="social" href="https://twitter.com/fwoelffel" target="_blank" rel="noopener noreferrer">
-                <FaTwitter size="4em"/>
-              </a>
-              <a className="social" href="https://github.com/fwoelffel" target="_blank" rel="noopener noreferrer">
-                <FaGithub size="4em"/>
-              </a>
-              <a className="social" href="https://frdricwoelffel.typeform.com/to/TgRdLj" target="_blank"
-                 rel="noopener noreferrer">
-                <FaEnvelope size="4em"/>
-              </a>
-            </div>
-          </div>
+const HeroComponent = ({ title, description }) => (
+  <section class="hero is-fullheight is-bold is-info">
+    <div class="hero-body has-text-centered">
+      <div class="container">
+        <h1 class="title is-1">{title}</h1>
+        <h2 class="subtitle">{description}</h2>
+        <div class="is-block">
+          <a className="social"
+             href="https://www.linkedin.com/in/fredericwoelffel/"
+             target="_blank"
+             rel="noopener noreferrer">
+            <FaLinkedin size="4em"/>
+          </a>
+          <a className="social"
+             href="https://twitter.com/fwoelffel"
+             target="_blank"
+             rel="noopener noreferrer">
+            <FaTwitter size="4em"/>
+          </a>
+          <a className="social"
+             href="https://github.com/fwoelffel"
+             target="_blank"
+             rel="noopener noreferrer">
+            <FaGithub size="4em"/>
+          </a>
+          <a className="social"
+             ref="https://frdricwoelffel.typeform.com/to/TgRdLj"
+             target="_blank"
+             rel="noopener noreferrer">
+            <FaEnvelope size="4em"/>
+          </a>
         </div>
-      </section>
-    )}
-  />
+      </div>
+    </div>
+  </section>
 )
 
 export default HeroComponent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,33 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroComponent from "../components/hero.component"
 import FooterComponent from "../components/footer.component"
+import { StaticQuery } from "gatsby"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Frederic Woelffel | Node.js engineer" keywords={[]} />
-    <HeroComponent/>
-    <FooterComponent/>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title,
+            description
+          }
+        }
+      }
+    `}
+    render={
+      data => (
+        <Layout>
+          <SEO title="Frederic Woelffel | Node.js engineer" keywords={[]} />
+          <HeroComponent
+            title={data.site.siteMetadata.title}
+            description={data.site.siteMetadata.description}
+          />
+          <FooterComponent/>
+        </Layout>
+      )
+    }>
+  </StaticQuery>
 )
 
 export default IndexPage
