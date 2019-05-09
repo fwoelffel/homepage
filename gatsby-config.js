dd883d11f1cb9ed4dd953ebe4c86d29b20282108
Frederic Woelffel <hello@fwoelffel.me>
2019-05-09 22:08:19 +0200

    chore: update pwa name

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Frederic Woelffel`,
+        short_name: `fwoelffel.me`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
