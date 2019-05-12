288ceef9f188113e5d95a4bd2ccbe67f372e8482
Frederic Woelffel <hello@fwoelffel.me>
2019-05-13 00:37:06 +0200

    fix(footer): correct last build time display

--- src/components/footer.component.js
+++ src/components/footer.component.js
@@ -4,7 +4,7 @@ const FooterComponent = ({ buildDate }) => (
   <div class="footer has-text-centered">
     <div class="container">
       <div class="has-background-warning has-text-weight-bold block">Disclaimer: This is still a work in progress.</div>
-      <div class="has-text-grey-light"> Last built {buildDate.toLocaleString()} at {buildDate.toLocaleTimeString()}.</div>
+      <div class="has-text-grey-light"> Last built {buildDate.toLocaleDateString()} at {buildDate.toLocaleTimeString()}.</div>
     </div>
   </div>
 )
