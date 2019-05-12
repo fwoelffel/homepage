ae66c86b5fde71aa8e1da5aaced1c6e2a75fd218
Frederic Woelffel <hello@fwoelffel.me>
2019-05-12 20:44:40 +0200

    feat: add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-139958086-1",
+      },
+    },
   ],
 }
